8973733c308b395ad6122ad89810eae9114dbeab
kanatdavid <david.kanat@hotmail.com>
2025-11-12 20:29:54 +0100

    Podfeed, pod

---
+++ Podd-Projekt/DDModels/Pod.cs
@@ -0,0 +1,23 @@
+﻿namespace DDModels
+{
+    public class Pod : PodFeed
+    {
+        public string EpisodeId { get; set; }
+        public string Title { get; set; }
+        public string Info { get; set; }
+        public int Duration { get; set; }
+
+        public Pod(string id, string pName, string[] participants, string description, string category,
+            string eId, string title,string info, int duration)
+            : base(id, pName, participants, description, category)
+        {
+            EpisodeId = id;
+            Title = title;
+            Info = info;
+            Duration = duration;
+        }
+
+
+
+    }
+}
---
+++ Podd-Projekt/DDModels/PodFeed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DDModels
+{
+    [BsonKnownTypes(typeof(Pod))]
+    public class PodFeed
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string PodName { get; set; }
+        public string[] Participants { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public PodFeed(string id, string pName, string[] participants, string description, string category)
+        {
+            Id = id;
+            PodName = pName;
+            Participants = participants;
+            Description = description;
+            Category = category;
+        }
+    }
+}
