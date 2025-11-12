362fed865d629216114fe769d1375827b4ccfe01
kanatdavid <david.kanat@hotmail.com>
2025-11-12 18:50:46 +0100

    IRepository

---
+++ Podd-Projekt/BBBusiness-Layer/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBBusiness_Layer
+{
+    public interface IRepository<T>
+    {
+        //C
+        Task AddAsync(T obj);
+        //R
+        Task<T?> GetOneAsync(string id);
+        //R
+        Task<List<T>> GetAllAsync();
+        //U
+        bool UpdateAsync(T obj);
+        //D
+        Task DeletAsync(string id);
+    }
+}
