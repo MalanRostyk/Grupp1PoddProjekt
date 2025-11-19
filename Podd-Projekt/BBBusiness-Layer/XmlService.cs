b94a8ef510c03bbf18ed975b0a2fefd3360ae8a0
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-11-19 12:16:27 +0100

    testar persistent xml

---
+++ Podd-Projekt/BBBusiness-Layer/XmlService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCData_Access_Layer;
+using DDModels;
+{
+    
+}
+
+namespace BBBusiness_Layer
+{
+    public class XmlService : IXmlService
+    {
+        XmlRepository xmlRepo;
+
+        public XmlService(XmlRepository xmlRepository)
+        {
+            xmlRepo = xmlRepository;
+        }
+
+
+        public async Task SavePodFeedToXml(PodFeed pf)
+        {
+            await xmlRepo.SerializePodFeed(pf);
+        }
+
+        public async Task<PodFeed> LoadPodFeedFromXml()
+        {
+            return await xmlRepo.DeserializePodFeed();
+        }
+    }
+}
---
+++ Podd-Projekt/CCData-Access-Layer/XmlRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDModels;
+using System.IO;
+using System.Xml.Serialization; 
+
+namespace CCData_Access_Layer
+{
+
+    public class XmlRepository : IXmlRepository
+    {
+        public PodFeed pf;
+        public XmlRepository(PodFeed pf)
+        {
+            this.pf = pf;
+        }
+
+        public async Task SerializePodFeed(PodFeed pf)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PodFeed));
+            await using FileStream fs = new FileStream("podfeed.xml", FileMode.Create, FileAccess.Write);
+            serializer.Serialize(fs, pf);
+        }
+
+        public async Task<PodFeed> DeserializePodFeed()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PodFeed));
+            await using FileStream fs = new FileStream("podfeed.xml", FileMode.Open, FileAccess.Read);
+            return (PodFeed)serializer.Deserialize(fs);
+        }
+    }
+}
