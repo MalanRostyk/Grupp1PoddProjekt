f8c8a13b7ece624d770bd339ace7c5a49d48089c
kanatdavid <david.kanat@hotmail.com>
2025-11-20 15:20:36 +0100

    stavfel fixat

--- Podd-Projekt/AAPresentation-Layer/Form1.cs
+++ Podd-Projekt/AAPresentation-Layer/Form1.cs
@@ -108,7 +108,7 @@ namespace AAPresentation_Layer
         {
             PodFeed instanceForNewId = new();
             pf = await pfService.GetTempPodFeedAsync(); //En feed att använda
-            pf.Id = instanceForNewIdn.Id;
+            pf.Id = instanceForNewId.Id;
             pf.Link = tbLink.Text; //Rss feed i form av länk användaren vill se
             pf.podList = await service.ReadAllPodAsync(pf); //Fyll listan med Pod objekt från länken
             await pfService.UpdateRecentlySearchedAsync(pf);
